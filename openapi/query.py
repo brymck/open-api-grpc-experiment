4d007daa0b96602601c61450b63ebaaefc8aee7b
Bryan McKelvey <bryan.mckelvey@gmail.com>
2018-12-31 01:57:17 +0900

    Make client example code more readable

--- openapi/query.py
+++ openapi/query.py
@@ -3,5 +3,6 @@ from greeter_client import ApiClient, Configuration, DefaultApi, Person
 configuration = Configuration()
 client = ApiClient(configuration)
 api = DefaultApi(client)
-greeting = api.greet(payload=Person(name='bryan'))
+me = Person(name='bryan')
+greeting = api.greet(me)
 print(greeting.message)
